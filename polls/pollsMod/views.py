315ad28a00963bd9b7c7280029beb80f36fd1306
Jacob Keen <jaclkeen@gmail.com>
2017-01-27 13:35:28 -0600

    added detail view, and modified function controlling the index and detail views

--- polls/pollsMod/views.py
+++ polls/pollsMod/views.py
@@ -1,12 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
+from .models import Question
 
 # This is the simplest view possible in Django. To call the view, we need to map it to a URL - and for this we need a URLconf.
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index'")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = { 'latest_question_list': latest_question_list }
+
+    return render(request, 'pollsMod/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question {0}".format(question_id))
+    question = get_object_or_404(Question, pk=question_id)
+
+    model = { 'question': question }
+
+    return render(request, 'pollsMod/detail.html', model)
 
 def results(requst, question_id):
     response = "Your looking at the results of question %s."
